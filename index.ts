936ab0ebc0d77b276e704bc41c4886035c70cf83
Victor Blaess <victor.blaess@basf.com>
2023-05-10 17:15:39 +0200

    first successful test

--- index.ts
+++ index.ts
@@ -3,26 +3,34 @@ import {OpenAI} from 'langchain/llms/openai';
 import {SqlDatabase} from 'langchain/sql_db';
 import {SqlDatabaseChain} from 'langchain/chains';
 import * as dotenv from 'dotenv';
+import {env} from 'process';
 
 dotenv.config();
 
 export const run = async () => {
-  const datasource = new DataSource({
-    type: 'sqlite',
-    database: 'northwind.db',
-  });
+  try {
+    const datasource = new DataSource({
+      type: 'sqlite',
+      database: 'northwind.db',
+    });
 
-  const db = await SqlDatabase.fromDataSourceParams({
-    appDataSource: datasource,
-    includesTables: ['Products'],
-  });
+    const db = await SqlDatabase.fromDataSourceParams({
+      appDataSource: datasource,
+      includesTables: ['Products'],
+    });
 
-  const chain = new SqlDatabaseChain({
-    llm: new OpenAI({temperature: 0}),
-    database: db,
-  });
+    const chain = new SqlDatabaseChain({
+      llm: new OpenAI({temperature: 0, openAIApiKey: env.OPENAI_API_KEY}),
+      database: db,
+    });
 
-  const res = await chain.run('What are the products listed in the database?');
-  console.log(res);
-  // There are 3503 tracks.
+    const res = await chain.run(
+      'How man products are existing in the database table?'
+    );
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+run();
